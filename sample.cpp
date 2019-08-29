eaff6848b469614aa2bff8be43ddd0548f3eade5
Olivier Giroux <ogiroux@gmail.com>
2019-08-29 14:18:47 -0700

    Tuning spinning v. fairness, work in progress

--- sample.cpp
+++ sample.cpp
@@ -22,98 +22,154 @@ THE SOFTWARE.
 
 */
 
+// WAIT / NOTIFY
+    //#define __NO_TABLE
+    //#define __NO_FUTEX
+    //#define __NO_CONDVAR
+    //#define __NO_SLEEP
+    //#define __NO_IDENT
+    // To benchmark against spinning
+    //#define __NO_SPIN
+    //#define __NO_WAIT
+
+// SEMAPHORE
+    //#define __NO_SEM
+    //#define __NO_SEM_BACK
+    //#define __NO_SEM_FRONT
+    //#define __NO_SEM_POLL
+
+#include <cmath>
 #include <mutex>
 #include <thread>
 #include <vector>
 #include <string>
 #include <iostream>
+#include <iomanip>
+#include <numeric>
+#include <tuple>
 
 #include "sample.hpp"
 
 static constexpr int sections = 1 << 20;
 
-template <class F>
-void test_body(int threads, F && f) {
+using sum_mean_dev_t = std::tuple<int, double, double>;
 
+template<class V>
+sum_mean_dev_t sum_mean_dev(V && v) {
+    assert(!v.empty());
+    auto const sum = std::accumulate(v.begin(), v.end(), 0.0);
+    auto const mean = sum / v.size();
+    auto const sq_diff_sum = std::accumulate(v.begin(), v.end(), 0.0, [=](auto left, auto right) -> auto {
+        return left + (right - mean) * (right - mean);
+    });
+    auto const variance = sq_diff_sum / v.size();
+    auto const stddev = std::sqrt(variance);
+    return std::tie(sum, mean, stddev);
+}
+
+template <class F>
+sum_mean_dev_t test_body(int threads, F && f) {
+
+    std::vector<int> progress(threads, 0);
 	std::vector<std::thread> ts(threads);
-	for (auto& t : ts)
-		t = std::thread([=]() {
-            f(sections / threads);
+	for (int i = 0; i < threads; ++i)
+		ts[i] = std::thread([&, i]() {
+            progress[i] = f(sections / threads);
         });
 
 	for (auto& t : ts)
 		t.join();
+
+    return sum_mean_dev(progress);
 }
 
 template <class F>
-void test_omp_body(int threads, F && f) {
+sum_mean_dev_t test_omp_body(int threads, F && f) {
 #ifdef _OPENMP
-    #pragma omp parallel num_threads(threads)
-    {
-        f(sections / 16);
-    }
+    std::vector<int> progress(threads, 0);
+    #pragma omp parallel_for num_threads(threads)
+    for (int i = 0; i < threads; ++i)
+        progress[i] = f(sections / threads);
+    return mean_dev(progress);
 #else
-    assert(0);
+    assert(0); // build with -fopenmp
 #endif
 }
 
 template <class F>
-void test(std::string const& name, int threads, F && f, bool use_omp = false) {
+void test(std::string const& name, int threads, F && f, std::atomic<bool>& keep_going, bool use_omp = false) {
 
-	auto const t1 = std::chrono::steady_clock::now();
+    std::thread test_helper([&]() {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        keep_going.store(false, std::memory_order_relaxed);
+    });
 
-    if(use_omp)
-        test_omp_body(threads, f);
-    else
-        test_body(threads, f);
+    auto const t1 = std::chrono::steady_clock::now();
+    auto const smd = use_omp ? test_omp_body(threads, f)
+                             : test_body(threads, f);
+    auto const t2 = std::chrono::steady_clock::now();
 
-	auto const t2 = std::chrono::steady_clock::now();
+    test_helper.join();
 
 	double const d = double(std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count());
-	std::cout << name << " : " << d / sections << "ns per section." << std::endl;
+    std::cout << std::setprecision(2) << std::fixed;
+	std::cout << name << " : " << d / std::get<0>(smd) << "ns per step, fairness metric = " 
+                         << 100 * (1 - std::get<2>(smd) / std::get<1>(smd)) << "%." 
+                         << std::endl;
+}
+
+template<class F>
+void test_loop(F && f) {
+    static int const max = std::thread::hardware_concurrency();
+    static std::vector<std::pair<int, std::string>> const counts = 
+        { { 1, "1 thread" }, 
+          { 2, "2 threads" },
+          { max, "full occupancy" },
+#if !defined(__NO_SPIN) || !defined(__NO_WAIT)
+          { max * 2, "double occupancy" } 
+#endif
+        };
+    for(auto const& c : counts)
+        f(c);
 }
 
 template<class M>
 void test_mutex(std::string const& name, bool use_omp = false) {
-
-    int const max = std::thread::hardware_concurrency();
-    std::vector<std::pair<int, std::string>> const counts = 
-        { { 1, "1 thread" }, 
-          { 2, "2 threads" },
-          { max, "full occupancy" },
-          { max * 2, "double occupancy" } };
-    for(auto const& c : counts) {
+    test_loop([&](auto c) {
         M m;
-        auto f = [&](int n) {
-            for (int i = 0; i < n; ++i) {
+        std::atomic<bool> keep_going(true);
+        auto f = [&](int n) -> int {
+            int i = 0;
+            while(keep_going.load(std::memory_order_relaxed)) {
                 m.lock();
+                ++i;
                 m.unlock();
             }
+            return i;
         };
-        test(name + ": " + c.second, c.first, f);
-    }
+        test(name + ": " + c.second, c.first, f, keep_going);
+    });
 };
 
 template<class B>
 void test_barrier(std::string const& name, bool use_omp = false) {
 
-    int const max = std::thread::hardware_concurrency();
-    std::vector<std::pair<int, std::string>> const counts = 
-        { { 1, "1 thread" }, 
-          { 2, "2 threads" },
-          { max, "full occupancy" },
-          { max * 2, "double occupancy" } };
-    for(auto const& c : counts) {
+    test_loop([&](auto c) {
         B b(c.first);
-        auto f = [&](int n) {
+        std::atomic<bool> keep_going(true); // unused here
+        auto f = [&](int n)  -> int {
             for (int i = 0; i < n; ++i)
                 b.arrive_and_wait();
+            return n;
         };
-        test(name + ": " + c.second, c.first, f);
-    }
+        test(name + ": " + c.second, c.first, f, keep_going, use_omp);
+    });
 };
 
 int main() {
+
+    int const max = std::thread::hardware_concurrency();
+    std::cout << "System has " << max << " hardware threads." << std::endl;
 
 #ifndef __NO_MUTEX
     test_mutex<mutex>("Spinlock");
